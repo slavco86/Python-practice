5ca51c260486b8400abe7b0e4249c990f6693cad
SlavCO <slavick.slavick@gmail.com>
2018-01-08 21:10:05 +0000

    Working on comma code

---
+++ Comma Code.py
@@ -0,0 +1,20 @@
+def convert(atr):
+    newList = ''
+    
+    for i in atr:
+        if atr.index(i) < len(atr)-1:
+            newList += i + ', '
+        else:
+            newList += 'and ' + i
+    return newList
+        
+
+words = []
+while True:
+    print('Please enter word number ' + str(len(words)+1) + '(or enter nothing to stop):')
+    word = input()
+    if word == '':
+        print('Your collection of words is: ' + str(convert(words)))
+        words = []
+        break
+    words = words + [word] 
