fd6d3eabad9752d4fc4f67f0295b8f6f5cb0121e
SlavCO <slavick.slavick@gmail.com>
2018-01-08 21:10:24 +0000

    coma code

--- Comma Code.py
+++ Comma Code.py
@@ -1,14 +1,11 @@
 def convert(atr):
     newList = ''
-    
     for i in atr:
         if atr.index(i) < len(atr)-1:
             newList += i + ', '
         else:
             newList += 'and ' + i
     return newList
-        
-
 words = []
 while True:
     print('Please enter word number ' + str(len(words)+1) + '(or enter nothing to stop):')
