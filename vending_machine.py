abea7984d36f2d3d032d0c04f0deb4dbc374f224
slavco86 <slavick.slavick@gmail.com>
2018-01-23 07:04:07 +0000

    updated gitignore, working on test_vending_machine

--- vending_machine.py
+++ vending_machine.py
@@ -3,8 +3,9 @@ coins = [100, 50, 20, 10, 5, 2, 1]
 
 def give_change(amount):
     change = []
-    for coin in coins:
-        if coin <= amount:
-            amount -= coin
-            change.append(coin)
+    while amount > 0:
+        for coin in coins:
+            if coin <= amount:
+                amount -= coin
+                change.append(coin)
     return change
